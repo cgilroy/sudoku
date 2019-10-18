61344b8f386b2b64bfe7316f17e0c74acc9d5c15
CJ Gilroy <cjgilroy@MacBook-Pro.local>
2019-10-17 19:58:47 -0600

    Add render method

--- Board.rb
+++ Board.rb
@@ -1,4 +1,5 @@
 require('byebug')
+require_relative('Tile')
 
 class Board
 
@@ -13,10 +14,15 @@ class Board
             f.each_line.with_index do |line,idx|
                 line.chomp!
                 line.each_char do |char|
-                    arr[idx] << char
+                    if char != '0'
+                        arr[idx] << Tile.new(char.to_i,true)
+                    else
+                        arr[idx] << Tile.new(char.to_i,false)
+                    end
                 end
             end
         end
+        arr
     end
 
     def update_tile_val(pos)
@@ -24,7 +30,23 @@ class Board
     end
 
     def render
-
+        @grid.each_with_index do |row,idx|
+            row.each_with_index do |tile,idx|
+                # debugger
+                if (idx+1) % 3 == 0 && idx != 8
+                    print tile.value.to_s + "|"
+                else
+                    print tile.value.to_s + " "
+                end
+            end
+            if (idx+1) % 3 == 0 && idx != 8
+                puts
+                17.times do
+                    print "-"
+                end
+            end
+            puts
+        end
     end
 
     def solved?
@@ -34,3 +56,5 @@ class Board
 end
 
 x = Board.from_file('./puzzles/sudoku1.txt')
+y = Board.new(x)
+y.render
