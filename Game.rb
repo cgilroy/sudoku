15f499a1e9ad66a8252691684aa6c08deca346f6
CJ Gilroy <c.gilroy9@gmail.com>
2019-10-18 11:47:52 -0600

    Prompt for input and verify input

--- Game.rb
+++ Game.rb
@@ -1,15 +1,35 @@
+require('colorize')
 require_relative('Board')
 
 class Game
     def initialize
-        puzzle_arr = Board.from_file('./puzzles/sudoku1_solved.txt')
+        puzzle_arr = Board.from_file('./puzzles/sudoku1.txt')
         @board = Board.new(puzzle_arr)
     end
 
     def play
         until @board.solved?
-
+            system('clear')
+            @board.render
+            input = self.prompt
+            if !@board.position_okay?(input)
+                puts "Invalid position"
+                sleep(1)
+            elsif !@board.value_okay?(input)
+                puts "Invalid value"
+                sleep(1)
+            else
+                @board.update_tile_val(input)
+            end
         end
+    end
+
+    def prompt
+        puts "Please enter the position to change e.g. (1,2)"
+        pos = gets.chomp
+        puts "Please enter the value"
+        val = gets.chomp.to_i
+        return {"position" => pos, "value" => val}
     end
 
 end
